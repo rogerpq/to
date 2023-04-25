daedb77d6686a3138c7c526c1fdb84cafcaa4e42
روجر <104784913+rogerpq@users.noreply.github.com>
2023-04-25 19:24:46 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,10 @@
+import telebot
+from datetime import datetime
+bot = telebot.TeleBot("5558725774:AAEnwhCn5sHEobHy8Sp2xoZV5VvZUdczWJo")
+@bot.message_handler(commands=['/time'])
+def send_time(message):
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    current_date = now.strftime("%d/%m/%Y")
+    bot.reply_to(message, f"The current time is {current_time} on {current_date}")
+bot.polling()
